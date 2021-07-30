2b399ee7f42f42f210cdbe83a8d561e14a9175eb
Jonathan Henning <jhon.hnng@gmail.com>
2021-07-29 21:56:20 -0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Chart } from 'react-charts'
 
+import './App.css';
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const getRandomData = () => {
+		return [
+			{
+				data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
+			},
+			{
+				data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+			}
+		];
+	}
+
+	const series = React.useMemo(
+		() => ({
+		  type: 'bar'
+		}),
+		[]
+	  )
+ 
+	const axes = React.useMemo(
+		() => [
+			{ primary: true, type: 'linear', position: 'bottom' },
+			{ type: 'linear', position: 'left' }
+		],
+		[]
+	)
+ 
+	return (
+		<div className="grid">
+			<div>
+				<Chart data={getRandomData()} series={series} axes={axes} />
+			</div>
+			<div>
+				<Chart data={getRandomData()} axes={axes} />
+			</div>
+			<div>
+				<Chart data={getRandomData()} axes={axes} />
+			</div>
+			<div>
+				<Chart data={getRandomData()} axes={axes} />
+			</div>
+			<div>
+				<Chart data={getRandomData()} axes={axes} />
+			</div>
+			<div>
+				<Chart data={getRandomData()} axes={axes} />
+			</div>
+		</div>
+	)
 }
 
 export default App;
